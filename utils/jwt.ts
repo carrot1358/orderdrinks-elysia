251b2d4e814e457bdcca9a7987f2fee6d5f867a4
Nattapad Sa. <nattapad.sa@rmuti.ac.th>
2024-08-27 15:31:28 +0700

    make return raw decoded token default false, and remove console.log not using

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -17,14 +17,13 @@ export const sign = async ({ data, exp = Bun.env.JWT_EXPIRE || "7d" }: JWT) =>
 export const verify = async (jwt: string) =>
   (await jose.jwtVerify(jwt, secret)).payload;
 
-export const getUserIdFromToken = async (authorizationHeader: string, raw: boolean): Promise<string | DecodedToken> => {
+export const getUserIdFromToken = async (authorizationHeader: string, raw: boolean = false): Promise<string | DecodedToken> => {
   if (!authorizationHeader || !authorizationHeader.startsWith("Bearer")) {
     throw new Error("ไม่พบ token");
   }
 
   const token = authorizationHeader.split(" ")[1];
   const decodedToken = await jwt.verify(token) as {data: DecodedToken};
-  console.log("decodedToken", decodedToken);
 
   if (raw) {
     return decodedToken.data;
