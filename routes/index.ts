ecdbffa4ab89382549cc86f2987e4a9f386877d2
Nattapad Sa. <nattapad.sa@rmuti.ac.th>
2024-08-30 01:15:49 +0700

    add line auth

--- routes/index.ts
+++ routes/index.ts
@@ -3,3 +3,4 @@ export { default as productRoutes } from "./productRoutes";
 export { default as deviceRoutes } from "./deviceRoutes";
 export { default as orderRoutes } from "./orderRoutes";
 export { default as paymentRoutes } from "./paymentRoutes";
+export { default as lineAuthRoutes } from "./lineAuth";
