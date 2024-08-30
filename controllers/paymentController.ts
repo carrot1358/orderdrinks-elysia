e2142de383bd14fe623e1dbf638e3ede35c133f5
Nattapad Sa. <nattapad.sa@rmuti.ac.th>
2024-08-30 19:42:38 +0700

    rework and handle error on payment model

--- controllers/paymentController.ts
+++ controllers/paymentController.ts
@@ -12,13 +12,21 @@ export const createDefaultPayment = async () => {
 };
 
 export const getPayment = async (c: Context) => {
-  const payment = await Payment.find();
-  return {
-    status: 200,
-    success: true,
-    data: payment,
-    message: "ดึงข้อมูลการชำระเงินสำเร็จ",
-  };
+  try {
+    const payment = await Payment.find();
+    return {
+      status: 200,
+      success: true,
+      data: payment,
+      message: "ดึงข้อมูลการชำระเงินสำเร็จ",
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      success: false,
+      message: "ดึงข้อมูลการชำระเงินไม่สำเร็จ" + error,
+    };
+  }
 };
 
 export const updatePayment = async (c: Context) => {
@@ -27,14 +35,52 @@ export const updatePayment = async (c: Context) => {
     numberPromtpay: string;
   };
 
-  const payment = await Payment.updateOne({
-    typePromtpay,
-    numberPromtpay,
-  });
-  return {
-    status: 200,
-    success: true,
-    data: payment,
-    message: "อัปเดตข้อมูลการชำระเงินสำเร็จ",
-  };
+  if (typePromtpay === "phone") {
+    if (!numberPromtpay) {
+      return {
+        status: 400,
+        success: false,
+        message: "กรุณาระบุหมายเลขโทรศัพท์",
+      };
+    } else if (numberPromtpay.length !== 10) {
+      return {
+        status: 400,
+        success: false,
+        message: "หมายเลขโทรศัพท์ต้องมี 10 หลัก",
+      };
+    }
+  } else if (typePromtpay === "idCard") {
+    if (!numberPromtpay) {
+      return {
+        status: 400,
+        success: false,
+        message: "กรุณาระบุหมายเลขบัตรประชาชน",
+      };
+    } else if (numberPromtpay.length !== 13) {
+      return {
+        status: 400,
+        success: false,
+        message: "หมายเลขบัตรประชาชนต้องมี 13 หลัก",
+      };
+    }
+  }
+
+  try {
+    const payment = await Payment.updateOne({
+      typePromtpay,
+      numberPromtpay,
+    });
+    return {
+      status: 200,
+      success: true,
+      data: payment,
+      message: "อัปเดตข้อมูลการชำระเงินสำเร็จ",
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      success: false,
+      message: "อัปเดตข้อมูลการชำระเงินไม่สำเร็จ" + error,
+    };
+  }
 };
--- routes/paymentRoutes.ts
+++ routes/paymentRoutes.ts
@@ -1,21 +1,31 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { getPayment, updatePayment } from "~/controllers/paymentController";
+import { admin, auth } from "~/middlewares";
 
 const paymentRoutes = (app: Elysia) => {
   app.group("/api/v1/payments", (app) =>
     app
       .get("/", getPayment, {
+        beforeHandle: (c) => auth(c),
         detail: {
           tags: ["Payment"],
           summary: "ดึงข้อมูลการชำระเงินทั้งหมด",
           description: "ดึงข้อมูลการชำระเงินทั้งหมดในระบบ",
+          security: [{ bearerAuth: [] }],
         },
       })
       .put("/", updatePayment, {
+        beforeHandle: (c) => admin(c),
+        body: t.Object({
+          typePromtpay: t.String(),
+          numberPromtpay: t.String(),
+        }),
+        type: "formdata",
         detail: {
           tags: ["Payment"],
           summary: "อัปเดตข้อมูลการชำระเงิน",
           description: "อัปเดตข้อมูลการชำระเงินในระบบ",
+          security: [{ bearerAuth: [] }],
         },
       })
   );
