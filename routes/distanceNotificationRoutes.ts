3d0b26273abcf0063407b4078e6a681f0deafbd4
Nattapad Sa. <nattapad.sa@rmuti.ac.th>
2024-09-27 19:59:40 +0700

    add group on distance notifications

--- routes/distanceNotificationRoutes.ts
+++ routes/distanceNotificationRoutes.ts
@@ -8,39 +8,43 @@ import {
 import { driver } from "~/middlewares";
 
 const distanceNotificationRoutes = (app: Elysia) => {
-  return app
-    .get("/distance-notifications", getDistanceNotifications, {
-      beforeHandle: (c) => driver(c),
-      detail: {
-        tags: ["DistanceNotification"],
-        summary: "ดึงข้อมูลการแจ้งเตือนระยะทางทั้งหมด",
-        security: [{ bearerAuth: [] }],
-      },
-    })
-    .post("/distance-notifications", createDistanceNotification, {
-      beforeHandle: (c) => driver(c),
-      detail: {
-        tags: ["DistanceNotification"],
-        summary: "สร้างการแจ้งเตือนระยะทางใหม่",
-        security: [{ bearerAuth: [] }],
-      },
-    })
-    .put("/distance-notifications/:id", updateDistanceNotification, {
-      beforeHandle: (c) => driver(c),
-      detail: {
-        tags: ["DistanceNotification"],
-        summary: "อัปเดตการแจ้งเตือนระยะทาง",
-        security: [{ bearerAuth: [] }],
-      },
-    })
-    .delete("/distance-notifications/:id", deleteDistanceNotification, {
-      beforeHandle: (c) => driver(c),
-      detail: {
-        tags: ["DistanceNotification"],
-        summary: "ลบการแจ้งเตือนระยะทาง",
-        security: [{ bearerAuth: [] }],
-      },
-    });
+  app.group("/api/v1/distance-notifications", (app) =>
+    app
+      .get("/distance-notifications", getDistanceNotifications, {
+        beforeHandle: (c) => driver(c),
+        detail: {
+          tags: ["DistanceNotification"],
+          summary: "ดึงข้อมูลการแจ้งเตือนระยะทางทั้งหมด",
+          security: [{ bearerAuth: [] }],
+        },
+      })
+      .post("/distance-notifications", createDistanceNotification, {
+        beforeHandle: (c) => driver(c),
+        detail: {
+          tags: ["DistanceNotification"],
+          summary: "สร้างการแจ้งเตือนระยะทางใหม่",
+          security: [{ bearerAuth: [] }],
+        },
+      })
+      .put("/distance-notifications/:id", updateDistanceNotification, {
+        beforeHandle: (c) => driver(c),
+        detail: {
+          tags: ["DistanceNotification"],
+          summary: "อัปเดตการแจ้งเตือนระยะทาง",
+          security: [{ bearerAuth: [] }],
+        },
+      })
+      .delete("/distance-notifications/:id", deleteDistanceNotification, {
+        beforeHandle: (c) => driver(c),
+        detail: {
+          tags: ["DistanceNotification"],
+          summary: "ลบการแจ้งเตือนระยะทาง",
+          security: [{ bearerAuth: [] }],
+        },
+      })
+  );
+
+  return app;
 };
 
 export default distanceNotificationRoutes;
